18425b3ad42238a56e583ff823ece6b25b272ec8
Arsya Aulia <arsya1522@gmail.com>
2025-06-18 08:44:22 +0800

    modified

--- public/script.js
+++ public/script.js
@@ -68,6 +68,33 @@ function setLevel(level) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Darkmode
+    const themeToggle = document.getElementById('themeToggle');
+    const html = document.documentElement;
+    const icon = themeToggle.querySelector('i');
+
+    // Check for saved theme preference
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        html.classList.add('dark');
+        icon.classList.replace('fa-moon', 'fa-sun');
+    }
+
+    // Toggle theme
+    themeToggle.addEventListener('click', function() {
+        html.classList.toggle('dark');
+        
+        // Update icon
+        if (html.classList.contains('dark')) {
+            icon.classList.replace('fa-moon', 'fa-sun');
+            localStorage.theme = 'dark';
+        } else {
+            icon.classList.replace('fa-sun', 'fa-moon');
+            localStorage.theme = 'light';
+        }
+    });
+
+    
     const openPopupBtn = document.getElementById('openPopup');
     const closePopupBtn = document.getElementById('closePopup');
     const popupOverlay = document.getElementById('popupOverlay');
